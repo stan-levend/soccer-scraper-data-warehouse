c3ec0261689a024945bd6ed3a385b4c3c5fe31f8
Peter Kupka <peter.kupka@student.tuke.sk>
2021-12-01 19:36:47 +0100

    Upload New File

---
+++ star_schema/excel_queries.py
@@ -0,0 +1,24 @@
+#zlte/cervene karty nadobudnute v druhom respektive prvom polcase jednotlivych timov napriec ligami
+yellow_red_cards_halftime='''
+SELECT 
+'''
+
+#drill down -> zlte/cervene karty nadobudnute v jednotlivych minutach jednotlivych timov napriec ligamidrill down -> zlte/cervene karty nadobudnute v jednotlivych minutach jednotlivych timov napriec ligami
+
+#pocet odohranych zapasov jednotlivych lig (jednej ligy?) v zavislosti od mesiacov roka ->poukazat na koronu
+
+#pocet golov jednotlivych timov ligy(alebo spolu ligove statistiky) v zavislosti na veku strelcov golov
+
+#pocet jednotlivych eventov v zavislosti na pozicii hraca za vsetky sezony pre jednotlive ligy
+
+#pocet striedani hracov v zavislosti na pozicii hraca napriec ligami
+
+#pocet odohranych minut v zavislosti na jednotlivych poziciach -> z toho vieme vyvodit zaver ci je muzstvo viac ofenzive alebo defenzivne a tak isto ci maju muzstva prebytok/nedostatok hracov na jednotlivych poziciach, ci ich obmienaju viac/menej - len nejakych konkretnych lig(tie co maju lineupy)
+
+#pocet odohranych minut v zavislosti na nationalite 
+
+#pocet odohranych minut v zavislosti na veku strelcov golov
+
+#pocet golov v zavislori od toho, ci su domaci tim
+
+#pocet kariet jednotlivych timov v zavisloti od stadionu
